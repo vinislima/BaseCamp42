a28e6aefac77039850cb51f8087b52936615d816
vida-sil <vida-sil@student.42sp.org.br>
2021-04-16 22:11:46 +0000

    ex01 2 vez

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,30 +6,23 @@
 /*   By: vida-sil <vida-sil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 21:45:43 by vida-sil          #+#    #+#             */
-/*   Updated: 2021/04/15 21:58:40 by vida-sil         ###   ########.fr       */
+/*   Updated: 2021/04/16 22:04:05 by vida-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	unsigned int	i;
+	int				diff;
 
 	i = 0;
-	while (s1[i] != '\0' && (i < n))
+	diff = 0;
+	while ((i < n) && !diff && (s1[i] != '\0') && (s2[i] != '\0'))
 	{
-		if (s2[i] == '\0')
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
-		else if (s2[i] > s1[i] || s1[i] > s2[i])
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
+		diff = (unsigned char)s1[i] - (unsigned char)s2[i];
 		i++;
 	}
-	if (s2[i] != '\0')
-	{
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	}
-	return (0);
+	if (i < n && !diff && (s1[i] == '\0' || s2[i] == '\0'))
+		diff = (unsigned char)s1[i] - (unsigned char)s2[i];
+	return (diff);
 }
